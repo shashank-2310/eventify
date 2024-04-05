83b4994361437b585e93400cf1586242d68e9797
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2024-04-05 16:10:06 +0530

    Minor changes

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from 'next/cache'
 
 import { connectToDatabase } from '@/lib/database'
 import User from '@/lib/database/models/user.model'
-import Order from '@/lib/database/models/category.model'
+import Order from '@/lib/database/models/order.model'
 import Event from '@/lib/database/models/event.model'
 import { handleError } from '@/lib/utils'
 
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb+srv://shashank:shashank2310@cluster0.xc9dsos.mongodb.net/?retryWrites=true&w=majority"
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) throw new Error("MongoDB URI is missing.")
+    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
         dbName: 'eventify',
