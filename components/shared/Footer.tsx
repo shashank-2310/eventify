7102d0f834133f69ee2254e3c83038b36df4122b
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2024-04-15 15:14:55 +0530

    Footer fixed

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <footer className="border-t">
-            <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+            <div className="flex-center wrapper md:flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
                 <Link href={"/"}>
                     <Image
                         src={"/assets/images/logo.svg"}
@@ -16,7 +16,7 @@ const Footer = () => {
                     />
                 </Link>
 
-                <p>2023 Evently. All Rights reserved.</p>
+                <p>2024 Evently. All Rights reserved.</p>
             </div>
         </footer>
     )
